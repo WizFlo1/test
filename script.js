64a198b55d69dd80161d8868359dac19f246fa31
WizFlo <f.pares@icloud.com>
2019-01-30 10:33:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict"
+let varibleName = value;
+const variableName = value;
+var variableName = value;
+let counter = 0
+while (counter<10){
+    let subCount = counter/2;
+    counter++;
+
+
+
+}
+{
+    alert ("Quel est ton nom?");
+    prompt("ton age",0;);
+
+}
+
+alert ("Begin") 
+alert("Who's there?"){
+
+}
+let message
+if (login=="Admin")="Begin"
